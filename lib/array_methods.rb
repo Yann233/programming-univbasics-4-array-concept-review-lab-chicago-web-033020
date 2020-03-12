9a4db10438515966548d1e0b1ea8309072da64fb
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-12 02:53:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,25 +7,25 @@
 #  nil
 #end
 #################################################
-def find_element_index(array,value_to_find)
-  i=0
-  while i<array.length do
-    if array[i]==value_to_find
-      return i
-    end
-    i+=1
-  end
-  nil
-end
-#################
 #def find_element_index(array,value_to_find)
-#  array.length.times do |count|
-#    if array[count]==value_to_find
-#      return count
+#  i=0
+#  while i<array.length do
+#    if array[i]==value_to_find
+#      return i
 #    end
-#    nil
+#    i+=1
 #  end
+#  nil
 #end
+#################
+def find_element_index(array,value_to_find)
+    array.length.times do |count|
+    if array[count]==value_to_find
+      return count
+    end
+    nil
+  end
+end
 ########################
 #def find_element_index(array,value_to_find)
 #array.find_index(value_to_find)
