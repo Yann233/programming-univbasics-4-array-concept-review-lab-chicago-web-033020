9fafca5077d2364ad7b47ebfd109da29592835a9
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-12 02:40:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,7 +17,14 @@
 #  end
 #  nil
 #end
-
+################
+def find_element_index(array,value_to_find)
+  array.length.times do |i|
+    if array[i]==value_to_find
+      return i
+    end
+    nil
+  end
 
 
 
