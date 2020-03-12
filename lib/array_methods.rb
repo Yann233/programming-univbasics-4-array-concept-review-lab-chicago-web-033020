5d10ce636ad30cdc6fd8aff67e4252bb173e4810
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-12 03:19:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -31,9 +31,9 @@ def find_element_index(array,value_to_find)
 array.find_index(value_to_find)
 end
 
-def find_max_value(array)
-  array.max()# Add your solution here
-end
+#def find_max_value(array)
+#  array.max()# Add your solution here
+#end
 
 #def find_max_value(array)
 #  #x=array[0]#the 1st # is the greatest one,compare it with each one
