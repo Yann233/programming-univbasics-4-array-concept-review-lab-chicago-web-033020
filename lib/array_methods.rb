7c402e046fc5680209e49ccbe5d3832dfddf649f
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-12 03:07:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -45,6 +45,15 @@ def find_max_value(array)
   x
 end
 
+#def find_min_value(array)
+#  array.min()# Add your solution here
+#end
 def find_min_value(array)
-  array.min()# Add your solution here
+  x=array[0]#the 1st # is the greatest one,compare it with each one
+  array.length.times {|i|
+    if array[i]>x
+      x=array[i]
+    end
+  }
+  x
 end
